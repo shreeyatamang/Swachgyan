f31e8dc1dcbf9290cf7ef6ca56b00074e3a9d07c
adicember <aditikasingh2001@gmail.com>
2024-05-19 14:13:48 +0545

    logo in nav updated

--- src/componenets/Navbar/Navbar.js
+++ src/componenets/Navbar/Navbar.js
@@ -1,37 +1,41 @@
-import { Link } from 'react-router-dom';
-import React from 'react';
-import './Navbar.css';
-import IntroIcon from '../../assests/IntroIcon.png';
-import gameIcon from '../../assests/gameIcon.png';
-import quizzesIcon from '../../assests/quizIcon.png';
-import KidsClubIcon from '../../assests/kcLogo.png'; 
-import learnmoreIcon from '../../assests/learnmoreIcon.png';
+import { Link } from "react-router-dom";
+import React from "react";
+import "./Navbar.css";
+import IntroIcon from "../../assests/IntroIcon.png";
+import gameIcon from "../../assests/gameIcon.png";
+import quizzesIcon from "../../assests/quizIcon.png";
+import KidsClubIcon from "../../assests/kcLogo.png";
+import learnmoreIcon from "../../assests/learnmoreIcon.png";
+import logo from '../../assests/logo.png'; 
 
 
 const Navbar = () => {
-  
   return (
-    <nav className='navbar'>
-      <div className='desktopMenu'>
-        <Link to="/" className='desktopMenuListItem'> 
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <Link to="/">
+          <img src={logo} alt="Logo" className="logo-icon" />
+        </Link>
+      </div>
+      <div className="desktopMenu">
+        <Link to="/" className="desktopMenuListItem">
           <img src={IntroIcon} alt="Home" className="menu-icon" />
           <span>Home</span>
         </Link>
-        <Link to="/Games" className='desktopMenuListItem'>
+        <Link to="/Games" className="desktopMenuListItem">
           <img src={gameIcon} alt="Game" className="menu-icon" />
           <span>Games</span>
         </Link>
-        <Link to="/quizzes" className='desktopMenuListItem'>
+        <Link to="/quizzes" className="desktopMenuListItem">
           <img src={quizzesIcon} alt="Quizzes" className="menu-icon" />
           <span>Quizzes</span>
         </Link>
-        <Link to="/learnmore" className='desktopMenuListItem'>
+        <Link to="/learnmore" className="desktopMenuListItem">
           <img src={learnmoreIcon} alt="learnmore" className="menu-icon" />
           <span>learnmore</span>
         </Link>
-       
-      
-        <Link to="/KidsClub" className='desktopMenuListItem'>
+
+        <Link to="/KidsClub" className="desktopMenuListItem">
           <img src={KidsClubIcon} alt="Kids Club" className="menu-icon" />
           <span>Kids Club</span>
         </Link>
@@ -41,10 +45,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-
-
-
-
-
-
